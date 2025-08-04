9665269b4b8f654fa81173d6db0b85308c75ccea
Tahmidul <ta278@cornell.edu>
2025-08-04 14:57:47 -0400

    Updated styling to tailwind v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,6 +17,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="min-h-screen flex items-center justify-center bg-[oklch(0.85_0.2_250)]">
+        <h1 className="text-4xl font-bold text-white">
+          Tailwind v4 via Vite plugin is working!
+        </h1>
+      </div>
     </>
   )
 }
